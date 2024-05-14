9157fbc285264f54f2dfce25387f055273b1508c
Leon Vințeanu <leonvintzeanu@protonmail.com>
2024-05-15 01:40:16 +0300

    USS-11 implemented mongodb connection logic

---
+++ backend/src/config/db.js
@@ -0,0 +1,63 @@
+const { MongoClient } = require("mongodb");
+const loggingConfig = require("./logging");
+const log = require("pino")(loggingConfig);
+
+const privateMethods = {
+  getConnectionString: () => {
+    if (process.env.NODE_ENV === "production" && !process.env.MONGO_URI) {
+      log.error("MONGO_URI environment variable is not set!");
+      return;
+    }
+
+    if (process.env.NODE_ENV === "production" && process.env.MONGO_URI) {
+      return process.env.MONGO_URI;
+    }
+
+    return "mongodb://localhost:27017";
+  },
+};
+
+const client = new MongoClient(privateMethods.getConnectionString(), {
+  minPoolSize: 2,
+  maxPoolSize: 10,
+});
+
+let db;
+
+const collections = {
+  Projects: {
+    getCollection: async () => {
+      return await db.collection("projects");
+    },
+  },
+};
+
+const publicMethods = {
+  connect: async () => {
+    const connectionString = privateMethods.getConnectionString();
+
+    if (!db) {
+      log.info("Initializing MongoDB client");
+
+      try {
+        await client.connect();
+        db = client.db("uss-db");
+      } catch (e) {
+        log.error(`DB connect error ${e}`);
+      } finally {
+        return db;
+      }
+    }
+  },
+  shutdown: async () => {
+    if (client && client.isConnected()) {
+      try {
+        await client.close();
+      } catch (e) {
+        log.error(`DB disconnect error: ${e}`);
+      }
+    }
+  },
+};
+
+module.exports = { ...publicMethods, ...collections };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const loggingConfig = require("./config/logging");
 const log = require("pino")(loggingConfig);
+const db = require("./config/db");
 
 const app = express();
 const port = 8080;
 
-app.get("/api", (req, res) => {
+const init = async () => {
+  await db.connect();
+};
+
+app.get("/api", async (req, res) => {
+  const projectsCollection = await db.Projects.getCollection();
+  await projectsCollection.insertOne({ test: "test" });
   res.send("Hello World!");
 });
 
+init();
 app.listen(port, () => {
   log.info(`Server is running on port ${port}`);
+});
+
+process.on("SIGINT", async () => {
+  await db.shutdown();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  await db.shutdown();
+  process.exit(0);
 });
