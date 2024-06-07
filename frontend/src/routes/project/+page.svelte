714def845e5bd80e039b8cdc5e672faae4cb7760
Leon Vințeanu <leonvintzeanu@protonmail.com>
2024-06-07 18:29:32 +0300

    progress spinner on project page

--- frontend/src/routes/project/+page.svelte
+++ frontend/src/routes/project/+page.svelte
@@ -11,7 +11,7 @@
 	import IastScan from '$lib/components/IastScan.svelte';
 	import { fetchWithToken } from '$lib/fetchWithToken';
 	import { onMount } from 'svelte';
-	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, ProgressRadial } from '@skeletonlabs/skeleton';
 	const queryparams = $page.url.searchParams;
 	const projectId = queryparams.get('projectId');
 
@@ -32,62 +32,65 @@
 	onMount(fetchProject);
 </script>
 
-<div>
-	<Breadcrumb customName={project?.name} />
-	<!-- <p>{JSON.stringify(project)}</p> -->
-	<div class="mx-7 mb-1 mt-3 flex items-center gap-4">
-		<h1 class="h1">
-			<span
-				class="from-secondary-500 to-warning-500 text-ellipsis bg-gradient-to-br box-decoration-clone bg-clip-text capitalize text-transparent"
-			>
-				{project.name}
-			</span>
-		</h1>
-		<button class="btn btn-icon variant-filled-secondary"><MdiEdit /></button>
-		<a class="btn btn-icon variant-filled-warning" href={project.zapUrl}><MdiLink /></a>
+{#if loading}
+	<div class="flex h-full w-full items-center justify-center">
+		<ProgressRadial />
 	</div>
-	<div class="text-surface-400 ml-8 flex items-center gap-2">
-		<p>URL: {project.zapUrl}</p>
-		<p>-</p>
-		<p>Created at {new Date(project.createdAt).toLocaleDateString()}</p>
+{:else}
+	<div>
+		<Breadcrumb customName={project?.name} />
+		<!-- <p>{JSON.stringify(project)}</p> -->
+		<div class="mx-7 mb-1 mt-3 flex items-center gap-4">
+			<h1 class="h1">
+				<span
+					class="from-secondary-500 to-warning-500 text-ellipsis bg-gradient-to-br box-decoration-clone bg-clip-text capitalize text-transparent"
+				>
+					{project.name}
+				</span>
+			</h1>
+			<button class="btn btn-icon variant-filled-secondary"><MdiEdit /></button>
+			<a class="btn btn-icon variant-filled-warning" href={project.zapUrl}><MdiLink /></a>
+		</div>
+		<div class="text-surface-400 ml-8 flex items-center gap-2">
+			<p>URL: {project.zapUrl}</p>
+			<p>-</p>
+			<p>Created at {new Date(project.createdAt).toLocaleDateString()}</p>
+		</div>
+		<div class="ml-7"></div>
 	</div>
-	<div class="ml-7"></div>
-</div>
 
-<div class="card mx-7 my-3 flex h-fit overflow-hidden">
-	<AppRail height="h-full">
-		<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-			<div class="flex justify-center">
-				<MdiCode class="text-3xl" />
-			</div>
-			<span>Code</span>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-			<div class="flex justify-center">
-				<MdiWeb class="text-3xl" />
-			</div>
-			<span>Web</span>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-			<svelte:fragment slot="lead">
+	<div class="card mx-7 my-3 flex h-fit overflow-hidden">
+		<AppRail height="h-full">
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
 				<div class="flex justify-center">
-					<MdiLayersSearchOutline class="text-3xl" />
+					<MdiCode class="text-3xl" />
 				</div>
-			</svelte:fragment>
-			<span>Composition</span>
-		</AppRailTile>
-	</AppRail>
+				<span>Code</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+				<div class="flex justify-center">
+					<MdiWeb class="text-3xl" />
+				</div>
+				<span>Web</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+				<svelte:fragment slot="lead">
+					<div class="flex justify-center">
+						<MdiLayersSearchOutline class="text-3xl" />
+					</div>
+				</svelte:fragment>
+				<span>Composition</span>
+			</AppRailTile>
+		</AppRail>
 
-	<div class="">
-		{#if currentTile === 0}
-			<!-- content here -->
-			<SastScan projectId={project._id} />
-		{:else if currentTile === 1}
-			<!-- else if content here -->
-			<IastScan projectId={project._id} />
+		<div class="">
+			{#if currentTile === 0}
+				<SastScan projectId={project._id} />
+			{:else if currentTile === 1}
+				<IastScan projectId={project._id} />
 			{:else if currentTile === 2}
-			<!-- else content here -->
-			<ScaScan projectId={project._id} />
-		{/if}
+				<ScaScan projectId={project._id} />
+			{/if}
+		</div>
 	</div>
-</div>
+{/if}
